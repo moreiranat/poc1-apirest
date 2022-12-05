c54ecee9aa1b8c94cbb4bfee8e5eadf30ea8b5a2
Nataly Lucena <natalylucena.pb@gmail.com>
2022-12-04 21:09:13 -0300

    Criando o projeto e adicionando as dependências

---
+++ src/main/java/com/api/nataly/poc1api/Poc1ApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.api.nataly.poc1api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Poc1ApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Poc1ApiRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/nataly/poc1api/Poc1ApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nataly.poc1api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Poc1ApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
