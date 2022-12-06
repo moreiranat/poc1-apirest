ee478c840342c01676d7bff760873af6470ce920
Nataly Lucena <natalylucena.pb@gmail.com>
2022-12-06 00:51:46 -0300

    Criando o DTO de Customer

---
+++ src/main/java/com/api/nataly/poc1api/presentation/dtos/CustomerDTO.java
@@ -0,0 +1,19 @@
+package com.api.nataly.poc1api.presentation.dtos;
+
+import com.api.nataly.poc1api.model.enums.PersonType;
+
+public class CustomerDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String documentNumber;
+
+    private PersonType personType;
+
+    private String phoneNumber;
+
+}
