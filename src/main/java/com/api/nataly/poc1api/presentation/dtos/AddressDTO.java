f540165faca7a230350c673fc4b1e1e856c05f4d
Nataly Lucena <natalylucena.pb@gmail.com>
2022-12-06 01:13:35 -0300

    Criando o DTO de Address

--- src/main/java/com/api/nataly/poc1api/presentation/dtos/AddressDTO.java
+++ src/main/java/com/api/nataly/poc1api/presentation/dtos/AddressDTO.java
@@ -1,2 +1,28 @@
-package com.api.nataly.poc1api.presentation.dtos;public class AddressDTO {
+package com.api.nataly.poc1api.presentation.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDTO {
+
+    private Long id;
+
+    private String street;
+
+    private String number;
+
+    private String neighborhood;
+
+    private String city;
+
+    private String cep;
+
+    private String state;
+
+    private Long customerId;
+
 }
